717c026c56a9f2d64b69005cffc7aac303b8ec70
KubilayAkyildiz <KubilayAkyldz@gmail.com>
2017-10-09 11:41:01 +0300

    Activity Yaşam Döngüsü Örneği

---
+++ Activity_Yasam_Dongusu/app/src/main/java/com/example/kubilay/activity_yasam_dongusu/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kubilay.activity_yasam_dongusu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Copyright (c) 2017-present, Kubilay Akyıldız
+All rights reserved.
+
+# Android Java Kodları
